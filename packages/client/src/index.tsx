66765c73b0db550bb17bdcac85f3c97a8ee5fe58
ohslyfox <patricksfinger@gmail.com>
2021-10-13 21:04:47 -0700

    remove unused resources

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,24 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
-
-// function TestComponent() {
-//   const { loading, error, data } = useQuery(test);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error</p>;
-
-//   console.log(data);
-//   return data.pokemon.map((pk: any) => <p>{pk.name}</p>);
-// }
 
 ReactDOM.render(
   <React.StrictMode>
@@ -29,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
