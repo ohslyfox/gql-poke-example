ef13557be4906594306fc48f9c7b39c333387d16
ohslyfox <patricksfinger@gmail.com>
2021-10-13 23:01:35 -0700

    use apollo-server-express

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,18 @@
 import "reflect-metadata";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { PokemonService } from "./lib/pokemon/pokemon-client";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { GqlContext } from "./types";
 import { buildTypeDefsAndResolvers } from "type-graphql";
 import { PokemonResolver } from "./resolvers/pokemon";
+import express from "express";
+import http from "http";
 
 const main = async (): Promise<void> => {
   const pokemonService = new PokemonService();
+
+  const app = express();
+  const httpServer = http.createServer(app);
 
   const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
     resolvers: [PokemonResolver],
@@ -22,10 +27,23 @@ const main = async (): Promise<void> => {
     },
   });
 
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  await server.start();
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: [
+        "*",
+        "http://localhost:3000",
+        "https://studio.apollographql.com",
+      ],
+      credentials: true,
+    },
   });
+
+  await new Promise((resolve) =>
+    httpServer.listen({ port: 4000 }, () => resolve(true))
+  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 };
 
 main();
